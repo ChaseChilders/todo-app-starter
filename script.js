d0f0502db3ce025438e4fe1b6b5943e399c04bdd
Chase Childers <chasechilders12@gmail.com>
2022-04-26 18:44:02 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // You can access the 'data' variable from the 'data.js' file, so long as 'data.js'
 // comes before 'script.js' in the HTML document
-console.log(data)
+console.log(todoItems);
 
 // Use the 'data' variable to create the appropriate elements and append them onto
 // the page.
+
+const incompleteTasks = document.querySelector("#incomplete-items");
+const completeTasks = document.querySelector("#complete-items");
+
+for (let items of todoItems) {
+  const incompleteItems = document.createElement("li");
+  const completeItems = document.createElement("li");
+  if (items.completed == true) {
+    completeItems.innerText = items.title;
+    completeTasks.appendChild(completeItems);
+  } else {
+    incompleteItems.innerText = items.title;
+    incompleteTasks.appendChild(incompleteItems);
+  }
+}
+
+// incompleteItems.innerText = items;
+// incompleteTasks.appendChild(incompleteItems);
+
+incompleteTasks.classList.add("list-group-item");
+completeTasks.classList.add("list-group-item");
